053a8e0be82ec49abfdc0f3e810c6c2d93205135
Foulishmidtery <117833895+Foulishmidtery@users.noreply.github.com>
2026-09-17 20:22:12 +0700

    Add Web Profile legacy mutation service methods

--- src/server/services/web-profile.service.js
+++ src/server/services/web-profile.service.js
@@ -3,4 +3,8 @@ import { webProfileRepository } from "@/server/repositories/web-profile.reposito
 export const webProfileService = {
   legacyList: () => webProfileRepository.legacyList(),
   legacyDetail: (id) => webProfileRepository.legacyDetail(id),
+  legacyUpdateTitle: (data) => webProfileRepository.legacyUpdateTitle(data),
+  legacyUpdateLogo: (data) => webProfileRepository.legacyUpdateLogo(data),
+  legacyUpdateHeader: (data) => webProfileRepository.legacyUpdateHeader(data),
+  legacyUpdateColor: (data) => webProfileRepository.legacyUpdateColor(data),
 };
