bdd7d2c6eda29b26866445a03c06c4f4828155b0
Foulishmidtery <117833895+Foulishmidtery@users.noreply.github.com>
2026-09-17 20:29:20 +0700

    Add KDEKS province profile response shaping

--- src/server/services/abouts.service.js
+++ src/server/services/abouts.service.js
@@ -6,6 +6,52 @@ export const aboutsService = {
   getKdeksAboutsList: () => aboutsRepository.getKdeksAboutsList(),
   getKdeksAbouts: () => aboutsRepository.getKdeksAbouts(),
   getKdeksHistory: () => aboutsRepository.getKdeksHistory(),
+  getKdeksAboutProvince: async (idProvince) => {
+    const row = await aboutsRepository.getKdeksProvinceProfile(idProvince);
+    if (!row) return [];
+    return [{
+      id: row?.id,
+      title: row?.title,
+      title_en: row?.title_en,
+      abouts: row?.abouts,
+      abouts_en: row?.abouts_en,
+      historys: row?.historys,
+      historys_en: row?.historys_en,
+      web_identity: row?.web_identity,
+      id_province: row?.id_province,
+      images: row?.images,
+      province_name: row?.province_name,
+      structure: row?.structure,
+      sk: row?.sk,
+      twitter: row?.twitter,
+      facebook: row?.facebook,
+      linkedin: row?.linkedin,
+      instagram: row?.instagram,
+      youtube: row?.youtube,
+      address: row?.address,
+      phone_number: row?.phone_number,
+      fax: row?.fax,
+      email: row?.email,
+      maps: row?.maps,
+      officials: row?.officials,
+    }];
+  },
+  getKdeksHistoryProvince: async (idProvince) => {
+    const row = await aboutsRepository.getKdeksProvinceProfile(idProvince);
+    if (!row) return [];
+    return [{
+      id: row?.id,
+      title: row?.title,
+      title_en: row?.title_en,
+      abouts: row?.abouts,
+      abouts_en: row?.abouts_en,
+      historys: row?.historys,
+      historys_en: row?.historys_en,
+      web_identity: row?.web_identity,
+      id_province: row?.id_province,
+      images: row?.images,
+    }];
+  },
   getAboutById: (id) => aboutsRepository.getAboutById(id),
   updateAbout: (id, data) => aboutsRepository.updateAbout(id, data),
   deleteAbout: (id) => aboutsRepository.deleteAbout(id),
