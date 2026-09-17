9a7e7b4495f5c370cd8f3d778a6b90bc3b18e61d
Foulishmidtery <117833895+Foulishmidtery@users.noreply.github.com>
2026-09-17 20:21:58 +0700

    Add Web Profile legacy mutation queries

--- src/server/repositories/web-profile.repository.js
+++ src/server/repositories/web-profile.repository.js
@@ -8,4 +8,32 @@ export const webProfileRepository = {
   async legacyDetail(id) {
     return (await db.query("SELECT * FROM web_profile where id = $1", [id])).rows;
   },
+
+  async legacyUpdateTitle(data) {
+    return db.query(
+      "UPDATE web_profile SET web_title=$1 where id = $2",
+      [data.web_title, data.id],
+    );
+  },
+
+  async legacyUpdateLogo(data) {
+    return db.query(
+      "UPDATE web_profile SET web_logo=$1 where id = $2",
+      [data.web_logo, data.id],
+    );
+  },
+
+  async legacyUpdateHeader(data) {
+    return db.query(
+      "UPDATE web_profile SET web_header=$1 where id = $2",
+      [data.web_header, data.id],
+    );
+  },
+
+  async legacyUpdateColor(data) {
+    return db.query(
+      "UPDATE web_profile SET web_color=$1 where id = $2",
+      [data.web_color, data.id],
+    );
+  },
 };
