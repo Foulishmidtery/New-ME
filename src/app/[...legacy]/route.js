982c26fc8ecd41a70d280195f4c4167ac72bef4e
Foulishmidtery <117833895+Foulishmidtery@users.noreply.github.com>
2026-09-17 20:24:49 +0700

    Route Menu and Submenu settings natively

--- src/app/[...legacy]/route.js
+++ src/app/[...legacy]/route.js
@@ -5,6 +5,7 @@ import { handleLegacyContacts } from "@/server/controllers/legacy-contacts.contr
 import { handleLegacyDataMenu } from "@/server/controllers/legacy-data-menu.controller";
 import { handleLegacyKdeksProfileRead } from "@/server/controllers/legacy-kdeks-profile-read.controller";
 import { handleLegacyMaps } from "@/server/controllers/legacy-maps.controller";
+import { handleLegacyMenuSettings } from "@/server/controllers/legacy-menu-settings.controller";
 import { handleLegacyProvince } from "@/server/controllers/legacy-province.controller";
 import { handleLegacyScopes } from "@/server/controllers/legacy-scopes.controller";
 import { handleLegacySocialMedia } from "@/server/controllers/legacy-social-media.controller";
@@ -38,6 +39,9 @@ async function dispatch(request) {
 
   const mapsResponse = await handleLegacyMaps(request);
   if (mapsResponse) return mapsResponse;
+
+  const menuSettingsResponse = await handleLegacyMenuSettings(request);
+  if (menuSettingsResponse) return menuSettingsResponse;
 
   const provinceResponse = await handleLegacyProvince(request);
   if (provinceResponse) return provinceResponse;
